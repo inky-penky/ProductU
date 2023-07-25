b5c043bc492db87a338732098763b7c3a0bb0f2b
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-07-25 08:51:58 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens:{ 
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      backgroundImage:{
+        'hero': "url('../public/bg.png')"
+      }
+    },
   },
   plugins: [],
 }
