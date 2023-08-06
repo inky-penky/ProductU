0dc40f3fbc653f8c85ae8026718f34353028b194
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-08-06 10:24:33 -0700

    Update slider.tsx

--- components/slider.tsx
+++ components/slider.tsx
@@ -2,7 +2,7 @@ import Marquee from "react-fast-marquee"
 
 export default function Slider() {
     return(
-        <div className='mt-8 border-style-none'>
+        <div className='hidden mt-8 border-style-none md:block'>
             <Marquee className='py-3 bg-gradient-to-r from-blue-100 to-indigo-200'>
               <div className='mx-5'>
                 <p className="m-0">Application open for Cohort 1</p>
