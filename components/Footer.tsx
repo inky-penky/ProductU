255409a6f36d6185ee2d4ecf1ec8da49a27998c4
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-08-06 10:02:53 -0700

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-black ">
+        <div className="lg:mx-[12rem] mx-[3rem]">
+            <div className="flex gap-[5rem] py-[2.5rem] lg:py-[4rem] border-b border-gray-300 flex-col lg:flex-row">
+                <div className="flex flex-col gap-2">
+                    <h3 className="text-white">ProductU</h3>
+                    <a href='#' className='text-blue-500'>Course Details</a>
+                    <a href='#' className='text-blue-500'>Why Us?</a>
+                    <a href='#' className='text-blue-500'>Pricing</a>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="text-white">Wanna know more?</h3>
+                    <a href='#' className='text-blue-500'>Contact</a>
+                </div>
+            </div>
+            <div className="py-5">
+                <p className="text-sm text-gray-300">© Product U</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/slider.tsx
@@ -0,0 +1,22 @@
+import Marquee from "react-fast-marquee"
+
+export default function Slider() {
+    return(
+        <div className='mt-8 border-style-none'>
+            <Marquee className='py-3 bg-gradient-to-r from-blue-100 to-indigo-200'>
+              <div className='mx-5'>
+                <p className="m-0">Application open for Cohort 1</p>
+              </div>
+              <div className='mx-5'>
+                <p className="m-0">Application open for Cohort 1</p>
+              </div>
+              <div className='mx-2'>
+                <p className='m-0 text-green-600'>&#9679;</p>
+              </div>
+              <div className='mx-5'>
+                <p className="m-0">Last date for enrollment - July 23</p>
+              </div>
+            </Marquee>
+        </div>
+    )
+}
