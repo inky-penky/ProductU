0c59f49c306fef6a00aad2a70af29b37c999664c
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-08-08 15:45:51 -0700

    Update ToLearn.tsx

--- components/ToLearn.tsx
+++ components/ToLearn.tsx
@@ -3,15 +3,15 @@ import Data from "./data/learnData"
 export default function ToLearn() {
     const course = Data.map((data, index)=>{
         return(
-            <div>
-                <div className="pl-10 m-0 p-6 pb-8 mb-6 w-[24rem] rounded-[1.5rem] bg-gradient-to-r from-blue-100 to-violet-100">
-                <h1 className="m-0 mb-2 font-extralight text-md">{data.week}</h1>
-                <h1 className="m-0 mb-4 w-auto font-semibold text-md">{data.title}</h1>
-                <ul className="pl-5">
+            <div key={data.id}>
+                <div className="tracking-wider pl-5 mx-[1rem] p-6 pb-8 mb-6 w-[18rem] rounded-[1.5rem] bg-gradient-to-r from-blue-100 to-violet-100">
+                <h1 className="m-0 mb-2 font-extralight text-sm">{data.week}</h1>
+                <h1 className="m-0 mb-4 w-full font-semibold text-sm">{data.title}</h1>
+                <ul className="pl-2">
                     {
                         (data.courses).map((topic=>{
                             return(
-                                <li className="m-0 mb-3 text-md font-extralight"><span className="text-blue-600">&#9679;  </span>{topic}</li>
+                                <li className="m-0 mb-3 text-sm font-extralight"><span className="text-blue-600">&#9679;  </span>{topic}</li>
                             )
                             }))
                     }
@@ -22,8 +22,8 @@ export default function ToLearn() {
         )
     })
     return(
-        <section className="m-0 w-auto flex flex-col justify-between items-center">
-            <h1 className="mb-20 font-semibold text-xl">What Will You Learn</h1>
+        <section className="m-0 w-full flex flex-col justify-between items-center">
+            <h1 className="mb-10 font-semibold text-xl">What Will You Learn</h1>
             {course}
         </section>
     )
