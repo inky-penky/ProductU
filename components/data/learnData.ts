594d9e6bffb9d450cebf27caab908187ea0ae3d4
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-08-11 07:35:36 +0100

    Update learnData.ts

--- components/data/learnData.ts
+++ components/data/learnData.ts
@@ -3,42 +3,42 @@ const Data = [
         id : 1,
         week: "Week 1",
         title: "Introduction to Product management",
-        courses: ["What is Product Management?", "B2B, B2C, D2C", "Cross-functional partners","Differences between PM jobs across companies (anecdotes)", "How to chose the right PM role?" ],
+        courses: [{id: 1, name: "What is Product Management?"}, {id: 2, name:"B2B, B2C, D2C"}, {id:3, name:"Cross-functional partners"},{id:4, name:"Differences between PM jobs across companies (anecdotes)"}, {id:5, name:"How to chose the right PM role?"}],
         Assignment : "Assignment 1"
     },
     {
         id : 2,
         week: "Week 2",
         title: "Strategic understanding of the company",
-        courses: ["Understanding your company's mission and strategic goals?", "Understanding your org/team goals and KPI's?", "Market and competitors"," Connection product outcomes and business outcomes"],
+        courses: [{id: 1, name:"Understanding your company's mission and strategic goals?"}, {id:2, name:"Understanding your org/team goals and KPI's?"}, {id:3, name: "Market and competitors"}, {id: 4, name: "Connection product outcomes and business outcomes"}],
         Assignment : "Assignment 2"
     },
     {
         id : 3,
         week: "Week 3",
         title: "Problem Discovery",
-        courses: ["User research"," Customer journey maps", "Wireframing"],
+        courses: [{id:1, name: "User research"},{id: 2, name: "Customer journey maps"}, {id: 3, name:"Wireframing"}],
         Assignment : "Assignment 3"
     },
     {
         id : 4,
         week: "Week 4",
         title: "Problem Framing",
-        courses: ["Ideation & Validation",  "Problem framing","Hypothesis Building"],
+        courses: [{id:1, name: "Ideation & Validation"}, {id: 2, name: "Problem framing"}, {id: 3, name: "Hypothesis Building"}],
         Assignment : "Assignment 4"
     },
     {
         id : 5,
         week: "Week 5",
         title: "Designing & Building a Prototype",
-        courses: ["UX design principles", "Hypothesis Building", "Rapid prototyping" ],
+        courses: [{id: 1, name: "UX design principles"}, {id: 2, name: "Hypothesis Building"}, {id: 3, name: "Rapid prototyping"}],
         Assignment : "Assignment 5"
     },
     {
         id : 6,
         week: "Week 6",
         title: "Success Metrics & Experimentation",
-        courses: ["understanding metrics", "Choosing what to measure","Experimentation"],
+        courses: [{id: 1, name: "understanding metrics"}, {id: 2, name: "Choosing what to measure"}, {id: 3, name: "Experimentation"}],
         Assignment : "Assignment 6"
     }
 ]
