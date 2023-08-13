a9dd4482576e17b320d8355a50239f4e21ae803b
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-08-13 15:02:27 +0100

    Update Mentors.tsx

--- components/Mentors.tsx
+++ components/Mentors.tsx
@@ -5,13 +5,13 @@ import Image from "next/image"
 export default function Mentors() {
     const Lecturers = Data.map((Mentor, index)=>{
         return(
-                <div key={index} className="w-[18rem] mb-4 border-2 shadow-md shadow-indigo-100 border-blue-100 p-5 px-12 rounded-[3rem] flex flex-col justify-center md:mb-[3rem] md:w-[13.5rem]">
+                <div key={index} className="w-[18rem] h-[25.5rem] mb-4 border-2 shadow-md shadow-indigo-100 border-blue-50 py-[2rem] px-12 rounded-[1.5rem] flex flex-col justify-center md:m-[0.5rem] md:mb-[3rem] md:w-auto">
                     <div className="mb-14 flex justify-items-center">
                         <Image className="border-teal-100 border-[1rem] rounded-full" src={Mentor.image} alt="" width={130}></Image>           
                     </div>
                     <div className="flex flex-col justify-items-center place-items-center">
-                        <h1 className="tracking-wider text-center font-bold text-xl mb-6">{Mentor.name}</h1>
-                        <p className="text-center text-md mb-6">{Mentor.title}</p>
+                        <h1 className="tracking-wide text-center font-normal text-md mb-6 md:w-full">{Mentor.name}</h1>
+                        <p className="text-center text-sm mb-6">{Mentor.title}</p>
                         <div className="">
                             <Image src={Mentor.compImage} alt=""></Image>
                         </div>
