f94f07d91d55103fbb47c1240782fc9a96071886
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-08-13 03:26:40 +0100

    Update ToLearn.tsx

--- components/ToLearn.tsx
+++ components/ToLearn.tsx
@@ -4,12 +4,12 @@ export default function ToLearn() {
     const course = Data.map((data, index)=>{
         return(
             <div key={data.id}>
-                <div className="tracking-wider pl-5 mx-[1rem] p-6 pb-8 mb-6 w-[18rem] rounded-[1.5rem] bg-gradient-to-r from-blue-100 to-violet-100">
+                <div className="tracking-wider pl-5 mx-[1rem] p-6 pb-8 mb-6 w-[18rem] h-[27rem] rounded-[1.5rem] bg-gradient-to-r from-blue-100 to-violet-100 md:w-[14rem]">
                 <h1 className="m-0 mb-2 font-extralight text-sm">{data.week}</h1>
                 <h1 className="m-0 mb-4 w-full font-semibold text-sm">{data.title}</h1>
                 <ul className="pl-2">
                     {
-                        (data.courses).map((topic=>{
+                        (data.courses).map((topic =>{
                             return(
                                 <li key={topic.id} className="m-0 mb-3 text-sm font-extralight"><span className="text-blue-600">&#9679;  </span>{topic.name}</li>
                             )
@@ -24,7 +24,7 @@ export default function ToLearn() {
     return(
         <section className="flex flex-col justify-between items-center m-0 mt-[10.5rem] w-full">
             <h1 className="mb-[4.5rem] font-semibold text-center text-xl">What Will You Learn</h1>
-            <div className="flex flex-col justify-between items-center md:grid md:grid-cols-2 md:-gap-x-[3rem]">
+            <div className="flex flex-col justify-between items-center md:grid md:grid-cols-3 md:-gap-x-[1.5rem]">
             {course}
             </div>
         </section>
